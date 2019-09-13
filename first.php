1a662c6202223269a3d8ce447d6d424c09e71696
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-09-12 20:08:02 -0400

    sample helloworld

---
+++ first.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<style>
+body {
+  background-color: violet;
+}
+
+h1 {
+  color: grey;
+  text-align: center;
+}
+</style>
+</head>
+<h1>First program</h1>
+<body>
+<?php
+print "Hello World!.... This is my first program";
+?>
+</body>
+</html>
