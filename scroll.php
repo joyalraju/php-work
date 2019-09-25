cc9bb0697e894589ef7ff52db5af94d428bef625
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-09-24 22:54:28 -0400

    scrolling text with image as  background

---
+++ scroll.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<style>
+body {
+  background-image: url('https://www.hollandamerica.com/content/dam/hal/marketing-assets/deals/save-on-sunshine/destinations/save-on-sunshine-caribbean-beach-palm-tree-coconut-01101-c007.jpg.image.1008.754.low.jpg');
+
+background-position:center;
+background-repeat:no-repeat;
+background-size:cover;
+}
+
+h1 {
+  color: red;
+  text-align: center;
+font-size:60px;
+}
+</style>
+</head>
+<h1>QUOTE OF THE DAY</h1>
+<body>
+<style>
+h2 {
+font-family:fantasy;
+font-size:40px;
+font-style:italic;
+position:bottom;
+}
+</style>
+<h2><marquee style="font-weight:bold" behavior="scroll" diection="left">They began swinging high and higher as the sky opened its arms wide</marquee></h2>
+</body>
+</html>
